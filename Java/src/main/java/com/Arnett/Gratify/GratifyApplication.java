1c3e1d9510809f44576c5f11646adfdddb1ad7ee
Jake Arnett <jakearnett@Jakes-MacBook-Air.local>
2024-08-19 16:46:22 -0400

    first commit

---
+++ Java/src/main/java/com/Arnett/Gratify/GratifyApplication.java
@@ -0,0 +1,13 @@
+package com.Arnett.Gratify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GratifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GratifyApplication.class, args);
+	}
+
+}
---
+++ Java/src/test/java/com/Arnett/Gratify/GratifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Arnett.Gratify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GratifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
